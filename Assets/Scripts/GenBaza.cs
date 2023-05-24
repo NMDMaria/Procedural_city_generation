0da13ebdbc1e2f57e7ee010c57eb003a7fe99d64
Placintescu Stefan <stefanplaci@gmail.com>
2023-05-24 16:48:25 +0300

    Generare baza

--- Assets/Scripts/GenBaza.cs
+++ Assets/Scripts/GenBaza.cs
@@ -8,21 +8,20 @@ public class GenBaza : MonoBehaviour
 {
     public GameObject baza;
     public GameObject parc;
+    public int tileSize = 3;
 
     // Start is called before the first frame update
     void Start()
     {
-       baza = GameObject.FindGameObjectsWithTag("Baza");
-       parc = GameObject.FindGameObjectsWithTag("Parc");
-
-       int x = parc.GetComponent<Parcuri>().x;
-       int y = parc.GetComponent<Parcuri>().y;
-
+        baza = GameObject.FindGameObjectWithTag("Baza");
+        parc = GameObject.FindGameObjectWithTag("Parc");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float x = parc.GetComponent<Parcuri>().x;
+        float y = parc.GetComponent<Parcuri>().y;
+        baza.transform.localScale = new Vector3(tileSize*x, 1, tileSize*y);
     }
 }
