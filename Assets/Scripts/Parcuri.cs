49d42082fe0410cc5eaf1d6a1074617fe3b6b3bb
Octi21 <80587811+Octi21@users.noreply.github.com>
2023-05-26 21:59:58 +0300

    made road

--- Assets/Scripts/Parcuri.cs
+++ Assets/Scripts/Parcuri.cs
@@ -1,7 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Collections.Specialized;
-//using System.Diagnostics;
 using System.Security.Principal;
 using UnityEngine;
 
@@ -39,16 +39,28 @@ public class Parcuri : MonoBehaviour
 
     }
 
+    private int maxEntries(int n)
+    {
+        int result = (int)Math.Floor(Math.Sqrt(n));
+        return result+ 1;
+    }
+
     // Update is called once per frame
     void Update()
     {
         if (Input.GetButtonDown("Jump"))
         {
+            deleteDrum();
+
             matrix = Generate(); 
             x = matrix.GetLength(0);
             y = matrix.GetLength(1);
             int n = x;
             int m = y;
+            int maxN = maxEntries(n);
+            int maxM = maxEntries(m);
+
+
 
             List<Vector2Int> topE = new List<Vector2Int>();
             List<Vector2Int> botE = new List<Vector2Int>();
@@ -62,29 +74,27 @@ public class Parcuri : MonoBehaviour
 
             for (int i = 0; i < n; i++)
             {
-                c1[i] = matrix[i, 0];
-                c2[i] = matrix[i, m - 1];
+                c1[i] = matrix[i, 0];    // coloana din stanga
+                c2[i] = matrix[i, m - 1];  // coloana din dreapta
             }
             for (int i = 0; i < m; i++)
             {
-                l1[i] = matrix[0, i];
-                l2[i] = matrix[n-1, i];
+                l1[i] = matrix[0, i];     // linia de sus
+                l2[i] = matrix[n-1, i];    // linia de jos 
             }
-            Debug.Log(" l1:");
-
-            //printArr(l1);
+            
 
 
             // l1
-            int replaceCount = UnityEngine.Random.Range(1, 4); // Random number between 1 and 3
+            int replaceCount = UnityEngine.Random.Range(1, maxM); // Random number between 1 and 2
             int replaced = 0;
 
-            Debug.Log("Nr. of roads: ");
-            Debug.Log(replaceCount);
+            //Debug.Log("Nr. of roads: ");
+            //Debug.Log(replaceCount);
             
             while (replaced < replaceCount)
             {
-                int randomPosition = UnityEngine.Random.Range(1, m);
+                int randomPosition = UnityEngine.Random.Range(1, m-1);
 
                 if(l1[randomPosition] == '0')
                 {
@@ -97,14 +107,17 @@ public class Parcuri : MonoBehaviour
                     
                 }
             }
+            Debug.Log(" l1:");
+
+            printArr(l1);
 
             //l2
-            replaceCount = UnityEngine.Random.Range(1, 4); // Random number between 1 and 3
-            replaced = 0;
+            replaceCount = UnityEngine.Random.Range(1, maxM); // Random number between 1 and 2
+             replaced = 0; 
 
             while (replaced < replaceCount)
             {
-                int randomPosition = UnityEngine.Random.Range(1, m);
+                int randomPosition = UnityEngine.Random.Range(1, m-1);
                 if (l2[randomPosition] == '0')
                 {
                     if (randomPosition > 0 && l2[randomPosition - 1] != 'D' && randomPosition < m - 1 && l2[randomPosition + 1] != 'D')
@@ -115,13 +128,16 @@ public class Parcuri : MonoBehaviour
                     }
                 }
             }
+            Debug.Log(" l2:");
 
-            replaceCount = UnityEngine.Random.Range(1, 4); // Random number between 1 and 3
-            replaced = 0;
+            printArr(l2);
+
+            replaceCount = UnityEngine.Random.Range(1, maxN); // Random number between 1 and 2
+             replaced = 0;
 
             while (replaced < replaceCount)
             {
-                int randomPosition = UnityEngine.Random.Range(1, n);
+                int randomPosition = UnityEngine.Random.Range(1, n-1);
                 if (c1[randomPosition] == '0')
                 {
                     if (randomPosition > 0 && c1[randomPosition - 1] != 'D' && randomPosition < m - 1 && c1[randomPosition + 1] != 'D')
@@ -134,12 +150,12 @@ public class Parcuri : MonoBehaviour
             }
 
 
-            replaceCount = UnityEngine.Random.Range(1, 4); // Random number between 1 and 3
-            replaced = 0;
+             replaceCount = UnityEngine.Random.Range(1, maxN); // Random number between 1 and 2
+             replaced = 0;
 
             while (replaced < replaceCount)
             {
-                int randomPosition = UnityEngine.Random.Range(1, n);
+                int randomPosition = UnityEngine.Random.Range(1, n-1);
                 if (c2[randomPosition] == '0')
                 {
                     if (randomPosition > 0 && c2[randomPosition - 1] != 'D' && randomPosition < m - 1 && c2[randomPosition + 1] != 'D')
@@ -152,6 +168,12 @@ public class Parcuri : MonoBehaviour
 
                 }
             }
+            Debug.Log(" c1:");
+
+            printArr(c1);
+            Debug.Log(" c2:");
+
+            printArr(c2);
 
 
             //Debug.Log("Updated c2:");
@@ -175,7 +197,7 @@ public class Parcuri : MonoBehaviour
 
             for (int i = 0; i < topE.Count; i++)
             {
-                int idx = Random.Range(0, botE.Count - 1);
+                int idx = UnityEngine.Random.Range(0, botE.Count - 1);
                 if (botE.Count <= 0)
                     break;
                 BFS.getPath(matrix, topE[i].x, topE[i].y, botE[idx].x, botE[idx].y);
@@ -184,7 +206,7 @@ public class Parcuri : MonoBehaviour
 
             for (int i = 0; i < leftE.Count; i++)
             {
-                int idx = Random.Range(0, rightE.Count - 1);
+                int idx = UnityEngine.Random.Range(0, rightE.Count - 1);
                 if (rightE.Count <= 0)
                     break;
                 BFS.getPath(matrix, leftE[i].x, leftE[i].y, rightE[idx].x, rightE[idx].y);
@@ -206,7 +228,7 @@ public class Parcuri : MonoBehaviour
 
             Debug.Log(output);
 
-            // genDrum();
+            genDrum();
         }
     }
 
@@ -217,11 +239,11 @@ public class Parcuri : MonoBehaviour
         // Define the entrance roads in the city by random.
         for (int n = 0; n < nrEntrances; n++)
         {
-            Vector2Int entrance = margin[Random.Range(0, margin.Count - 1)];
+            Vector2Int entrance = margin[UnityEngine.Random.Range(0, margin.Count - 1)];
 
             while (!shouldBeEntrance(entrance))
             {
-                entrance = margin[Random.Range(0, margin.Count - 1)];
+                entrance = margin[UnityEngine.Random.Range(0, margin.Count - 1)];
             }
 
             //   Got a point that could be an entrance, mark it as road
@@ -314,21 +336,41 @@ public class Parcuri : MonoBehaviour
         return perlinGenerator.Generate();
     }
 
+    List<GameObject> roadInstances = new List<GameObject>();
+    List<GameObject> grassInstances = new List<GameObject>();
     private void genDrum(){
         int tileSize = 3;
-        List<GameObject> roadInstances = new List<GameObject>();
         GameObject road;
+        GameObject grass;
         road = Resources.Load<GameObject>("Roads/road_black");
+        grass = Resources.Load<GameObject>("Roads/green");
         for (int i=0; i<x; ++i){
             for (int j=0; j<y; ++j){
                 if (matrix[i, j] == 'D'){
                     roadInstances.Add(Instantiate(road));
-                    roadInstances[roadInstances.Count - 1].transform.position = new Vector3(3*i, 1, 3*y);
+                    roadInstances[roadInstances.Count - 1].transform.position = new Vector3(3*i, 1, 3*j);
+                }
+                else if (matrix[i, j] == 'P')
+                {
+                    grassInstances.Add(Instantiate(grass));
+                    grassInstances[grassInstances.Count - 1].transform.position = new Vector3(3 * i, 1, 3 * j);
                 }
             }
         }
+
+    }
+
+    private void deleteDrum() {
+        for (int i = 0; i < roadInstances.Count; ++i)
+            Destroy(roadInstances[i]);
+        roadInstances.Clear();
+        for (int i = 0; i < grassInstances.Count; ++i)
+            Destroy(grassInstances[i]);
+        grassInstances.Clear();
     }
 }
+
+
 /*int[] l1 = new int[m];
           int[] l2 = new int[m];
           int[] c1 = new int[n];
