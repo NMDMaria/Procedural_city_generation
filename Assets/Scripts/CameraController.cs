8a5180ca4763a8f3e8dd6502040c0c00954d1f8d
Negruț Maria-Daniela <80623515+NMDMaria@users.noreply.github.com>
2023-05-27 15:43:19 +0300

    camera 2

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,24 +5,60 @@ public class CameraController : MonoBehaviour
     public float movementSpeed = 5f;
     public float rotationSpeed = 100f;
     public KeyCode activateKey = KeyCode.C;
-    
+    public KeyCode jumpKey = KeyCode.Space;
+    public PerlinGenerator perlinGenerator;
+    public float fieldOfView = 50f; // Camera's field of view
+
     private bool isActivated = false;
     private Vector3 topDownPosition;
     private Quaternion topDownRotation;
+    private int cityX;
+    private int cityY;
+    private Vector3 initialPosition;
+    private Quaternion initialRotation;
 
     void Start()
     {
+        cityX = perlinGenerator.textureX;
+        cityY = perlinGenerator.textureY;
+        float maxCityDimension = Mathf.Max(3 * cityX, 3 * cityY);
+        float cameraHeight = maxCityDimension / (2f * Mathf.Tan(fieldOfView * 0.5f * Mathf.Deg2Rad));
+
+        topDownPosition = new Vector3(3 * cityX / 2, cameraHeight, 3 * cityY / 2);
+
         // Store the top-down position and rotation of the camera
-        topDownPosition = new Vector3(0f, 30f, 0f);
         topDownRotation = Quaternion.Euler(90f, 0f, 0f);
-        
+
         // Set the initial camera position and rotation to top-down view
         transform.position = topDownPosition;
         transform.rotation = topDownRotation;
+
+        // Store the initial camera position
+        initialPosition = new Vector3(0f, 1f, 0f);
+        initialRotation = Quaternion.identity;
     }
 
     void Update()
     {
+        if (Input.GetButtonDown("Jump"))
+        {
+            // Reset the camera position to the initial position
+            transform.position = initialPosition;
+
+            cityX = perlinGenerator.textureX;
+            cityY = perlinGenerator.textureY;
+            float maxCityDimension = Mathf.Max(3 * cityX, 3 * cityY);
+            float cameraHeight = maxCityDimension / (2f * Mathf.Tan(fieldOfView * 0.5f * Mathf.Deg2Rad));
+
+            topDownPosition = new Vector3(3 * cityX / 2, cameraHeight, 3 * cityY / 2);
+
+            if (!isActivated)
+            {
+                transform.position = topDownPosition;
+                transform.rotation = topDownRotation;
+            }
+        }
+
         // Check if the activate key is pressed
         if (Input.GetKeyDown(activateKey))
         {
@@ -31,29 +67,39 @@ public class CameraController : MonoBehaviour
             if (isActivated)
             {
                 // Set the camera to player mode
-                transform.position = new Vector3(0f, 1f, 0f);
-                transform.rotation = Quaternion.identity;
+                transform.position = initialPosition;
+                transform.rotation = initialRotation;
             }
             else
             {
+                initialPosition = transform.position;
+                initialRotation = transform.rotation;
                 // Set the camera back to top-down view
                 transform.position = topDownPosition;
                 transform.rotation = topDownRotation;
             }
+
+            // Lock or unlock the cursor based on isActivated
+            Cursor.lockState = isActivated ? CursorLockMode.Locked : CursorLockMode.None;
+            Cursor.visible = !isActivated;
         }
 
         // Check if the camera controls are activated
         if (isActivated)
         {
-            // Camera rotation
-            float mouseX = Input.GetAxis("Mouse X") * rotationSpeed * Time.deltaTime;
-            float mouseY = Input.GetAxis("Mouse Y") * rotationSpeed * Time.deltaTime;
-            transform.Rotate(-mouseY, mouseX, 0f);
+            // Move the camera
+            float moveHorizontal = Input.GetAxis("Horizontal");
+            float moveVertical = Input.GetAxis("Vertical");
 
-            // Camera movement
-            float moveHorizontal = Input.GetAxis("Horizontal") * movementSpeed * Time.deltaTime;
-            float moveVertical = Input.GetAxis("Vertical") * movementSpeed * Time.deltaTime;
-            transform.Translate(moveHorizontal, 0f, moveVertical);
+            Vector3 moveDirection = new Vector3(moveHorizontal, 0f, moveVertical);
+            transform.Translate(moveDirection * movementSpeed * Time.deltaTime);
+
+            // Rotate the camera
+            float rotationX = Input.GetAxis("Mouse X");
+            float rotationY = Input.GetAxis("Mouse Y");
+
+            transform.Rotate(Vector3.up, rotationX * rotationSpeed * Time.deltaTime);
+            transform.Rotate(Vector3.left, rotationY * rotationSpeed * Time.deltaTime);
         }
     }
 }
